4c3d5fcdadda0efa7403d9d7f4cad6e23fdd649d
Himansu Mallik <dreamhimansu@gmail.com>
2026-04-12 21:28:23 +0530

    Updated Firebase config for Render

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,8 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
