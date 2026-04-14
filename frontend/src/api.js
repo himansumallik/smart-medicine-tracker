f29212b57143927ec2a990211f004ad7e0f3d7d3
Himansu Mallik <dreamhimansu@gmail.com>
2026-04-14 11:27:39 +0530

    fix api url env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,16 @@
 const BASE_URL = "http://43.205.130.165";
 
 export const addMedicine = async (data) => {
-  return await fetch(`${BASE_URL}/add-medicine`, {
+  const res = await fetch(`${BASE_URL}/add-medicine`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
   });
+
+  return res.json();
 };
 
 export const getMedicines = async (userId) => {
-  const res = await fetch(`${BASE_URL}/get-medicines/${userId}`);
+  const res = await fetch(`${BASE_URL}/medicines/${userId}`);
   return res.json();
 };
